60e371b213409c4d933372eaf7cd1d5599390d28
Nikita Ivanov <grafgenerator@gmail.com>
2015-06-21 13:05:45 +0600

    fixes typo and issues in code of index.js

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 'use strict';
-var gutil = require('gulp-util');
 var _ = require('lodash');
 
-const PLUGIN_NAME = "gulp-json-server";
-
 var gulpServerStart = function (options) {
-	/*if (typeof(dataSource) === "undefined") {
-		throw new gutil.PluginError(PLUGIN_NAME, 'Data source required to start the server');
-	}*/
-
 	var serverOptions = {
-		data: 'db.json'
+		data: 'db.json',
 		port: 3000
 	};
-	
+
 	_.assign(serverOptions, options || {});
-	
+
 	var jsonServer = require('json-server');
 	var server = jsonServer.create();
 
