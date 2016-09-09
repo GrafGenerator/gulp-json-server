357e682ec291263d2f188633199d6735c032b063
Nikita Ivanov <grafgenerator@gmail.com>
2016-09-09 23:11:43 +0700

    trying to debug only reload spec

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('jshint', function(){
 });
 
 gulp.task('test', function(){
-	return gulp.src('test/*.js', {read: false})
+	return gulp.src('test/reloadSpec.js', {read: false})
 		.pipe(mocha({reporter: 'spec'}));
 });
 
